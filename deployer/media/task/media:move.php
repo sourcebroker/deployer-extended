754c7041098ba4a4d14a6358dc295d712d94eb42
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-02-25 00:43:16 +0100

    [FEATURE] Add media:move and media:push task.

--- deployer/media/task/media:move.php
+++ deployer/media/task/media:move.php
@@ -3,10 +3,8 @@
 namespace Deployer;
 
 task('media:move', function () {
-
     $targetServer = get('db_get_local_server')->get('server')['name'];
 
     runLocally('{{bin/php}} {{bin/deployer}} media:pull ' . get('server')['name']);
     runLocally('{{bin/php}} {{bin/deployer}} media:push ' . $targetServer);
-
 })->desc('Synchronize media between instances.');
--- deployer/media/task/media:push.php
+++ deployer/media/task/media:push.php
@@ -5,14 +5,11 @@ namespace Deployer;
 task('media:push', function () {
     $config = array_merge_recursive(get('media-default'), get('media'));
 
-    // TODO - $src can not be callable like this - this is bug
     $src = get('deploy_path') . '/current';
     while (is_callable($src)) {
         $src = $src();
     }
 
-    print_r($src);
-    die(' -> 24.02.2017 17:32');
     if (!trim($src)) {
         throw new \RuntimeException('You need to specify a source path.');
     }
@@ -32,6 +29,5 @@ task('media:push', function () {
     $user = !$server->getUser() ? '' : $server->getUser() . '@';
 
     $flags = isset($config['flags']) ? '-' . $config['flags'] : false;
-    runLocally("rsync {$flags} -e 'ssh$port$identityFile' {{media_rsync_options}}{{media_rsync_includes}}{{media_rsync_excludes}}{{media_rsync_filter}} '$dst/' '$user$host:$src/' ",
-        0);
+    runLocally("rsync {$flags} -e 'ssh$port$identityFile' {{media_rsync_options}}{{media_rsync_includes}}{{media_rsync_excludes}}{{media_rsync_filter}} '$dst/' '$user$host:$src/' ", 0);
 })->desc('Synchronize media between instances.');
