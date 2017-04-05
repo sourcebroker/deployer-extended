b4f9fa33852ad9cdf45bd77e79e8032c60d4dada
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-04-05 11:33:24 +0200

    [TASK] Refactor "composer install" checj.

--- deployer/deploy/task/deploy:composer_install_check.php
+++ deployer/deploy/task/deploy:composer_install_check.php
@@ -3,9 +3,11 @@
 namespace Deployer;
 
 task('deploy:composer_install_check', function () {
-    exec('composer install --dry-run 2>&1 ', $output);
-    $outputInline = join(' ', $output);
-    if (strpos($outputInline, 'Nothing to install or update') === false) {
-        throw new \RuntimeException('Please run composer install command');
+    $composerBin = runLocally('which composer')->toString();
+    if (runLocally("if hash $composerBin 2>/dev/null; then echo 'true'; fi")->toBool()) {
+        $output = runLocally($composerBin . ' install --dry-run 2>&1');
+        if (strpos($output, 'Nothing to install or update') === false) {
+            throw new \RuntimeException('A composer.lock changes has been detected but you did not run "composer install". Please run composer install, then check if everything is working on your instance and do deploy after.');
+        }
     }
-})->desc('Check composer install output');
+})->desc('Check if composer install is needed.');
