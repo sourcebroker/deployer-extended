e2f55c166080bfb5bae99a1c18afd053e2448261
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-04-05 15:07:27 +0200

    [BUGFIX] Disable vhost overwrite

--- deployer/deploy/task/deploy:vhost.php
+++ deployer/deploy/task/deploy:vhost.php
@@ -25,14 +25,21 @@ task('deploy:vhost_apache', function () {
         } else {
             set('document_root', get('deploy_path') . '/current');
         }
+
         set('reponame', runLocally('basename `git rev-parse --show-toplevel`'));
+
         file_put_contents(parse('{{current_dir}}/{{reponame}}.conf'), parse(get('vhost_template')));
-        if (get('vhost_path', false) !== false) {
-            runLocally('mv {{current_dir}}/{{reponame}}.conf ' . FileUtility::getUniqueFileName(parse('{{vhost_path}}/{{reponame}}.conf')));
+
+        if (!file_exists(parse('{{vhost_path}}/{{reponame}}.conf'))) {
+            if (get('vhost_path', false) !== false) {
+                runLocally('mv {{current_dir}}/{{reponame}}.conf ' . FileUtility::getUniqueFileName(parse('{{vhost_path}}/{{reponame}}.conf')));
+            } else {
+                writeln(parse('You did not set "VHOST_PATH" env var so we do not know where to copy generated vhosts. This is why vhost was stored in {{current_dir}} so you can move it manually.'));
+            }
         } else {
-            throw new Exception('You did not set "vhost_path" var so we do not know where to copy generated vhosts. They were stored in {{deploy_path}}');
+            throw new Exception(parse('There is already vhost with the name {{reponame}}.conf at directory {{vhost_path}}. This is why vhost was stored in {{current_dir}} so you can move it manually.'));
         }
     } else {
         throw new Exception('"public_urls" var was not set for server. Vhost can not be generated without it.');
     }
-})->desc('Create vhost and copy to env path set in "vhost_path"');
+})->desc('Create vhost and copy to env path set in "VHOST_PATH"');
