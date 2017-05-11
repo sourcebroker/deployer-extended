3da2fc3be6815ed8a13130b627870d0f8561d8b0
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-05-12 00:06:05 +0200

    [TASK] Set --ignore-platform-reqs for check if composer install is required. Less problems iwth php versions but still main reason of this check is done.

--- deployer/deploy/task/deploy:composer_install_check.php
+++ deployer/deploy/task/deploy:composer_install_check.php
@@ -5,7 +5,7 @@ namespace Deployer;
 task('deploy:composer_install_check', function () {
     $composerBin = runLocally('which composer')->toString();
     if (runLocally("if hash $composerBin 2>/dev/null; then echo 'true'; fi")->toBool()) {
-        $output = runLocally($composerBin . ' install --dry-run 2>&1');
+        $output = runLocally($composerBin . ' --ignore-platform-reqs install --dry-run 2>&1');
         if (strpos($output, 'Nothing to install or update') === false) {
             throw new \RuntimeException('A composer.lock changes has been detected but you did not run "composer install". Please run composer install, then check if everything is working on your instance and do deploy after.');
         }
