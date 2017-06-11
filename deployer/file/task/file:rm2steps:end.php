652dfdf4c42ead08949e826e6cb4a579c472a8e0
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-11 20:33:49 +0200

    [TASK] Introduce file:rm2steps:start and file:rm2steps:end

---
+++ deployer/file/task/file:rm2steps:end.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Deployer;
+
+use SourceBroker\DeployerExtended\Utility\FileUtility;
+
+task('file:rm2steps:end', function () {
+    $removeRecursiveAtomicItems = get('file_remove2steps_items');
+    $random = get('random');
+    // Set active_path so the task can be used before or after "symlink" task or standalone.
+    $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
+    foreach ($removeRecursiveAtomicItems as $removeRecursiveAtomicItem) {
+        $removeRecursiveAtomicItem = escapeshellarg(FileUtility::normalizeFolder($removeRecursiveAtomicItem));
+        if (test("[ -e '$activePath/$removeRecursiveAtomicItem$random' ]")) {
+            run("cd '$activePath' && rm -rf '$removeRecursiveAtomicItem$random'");
+        }
+    }
+})->desc('Remove files and directories in two steps. First step: rename. Second step: remove.');
