0d0911874cb8c17cc3b767f1a4b56aebde7cef17
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-13 00:27:16 +0200

    [BUGFIX] Fix bug and refactor 2 step remove.

--- deployer/file/task/file:rm2steps:1.php
+++ deployer/file/task/file:rm2steps:1.php
@@ -10,9 +10,12 @@ task('file:rm2steps:1', function () {
     // Set active_path so the task can be used before or after "symlink" task or standalone.
     $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
     foreach ($removeRecursiveAtomicItems as $removeRecursiveAtomicItem) {
-        $removeRecursiveAtomicItem = escapeshellarg(FileUtility::normalizeFolder($removeRecursiveAtomicItem));
-        if (test("[ -e '$activePath/$removeRecursiveAtomicItem$random' ]")) {
-            run("cd '$activePath' && mv -f '$removeRecursiveAtomicItem' '$removeRecursiveAtomicItem$random'");
+        $removeRecursiveAtomicItem = FileUtility::normalizeFolder(trim($removeRecursiveAtomicItem));
+        if(strlen($removeRecursiveAtomicItem)) {
+            $itemToRename = escapeshellarg("$activePath/$removeRecursiveAtomicItem");
+            $itemToRenameNewName = escapeshellarg("$activePath/$removeRecursiveAtomicItem$random");
+            run("if [ -e $itemToRename ]; then mv -f $itemToRename $itemToRenameNewName; fi");
         }
+
     }
 })->desc('Remove files and directories in two steps. First step: rename. Second step: remove.');
