751fe6edc60e385b9fb067acdef8ebcba144fedd
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-15 12:58:46 +0200

    [BUGFIX] Add current_dir path to check deploy.lock

--- deployer/deploy/task/deploy:check_lock.php
+++ deployer/deploy/task/deploy:check_lock.php
@@ -3,8 +3,8 @@
 namespace Deployer;
 
 task('deploy:check_lock', function () {
-    if (file_exists('./deploy.lock')) {
-        $deployLockFileContent = trim(file_get_contents('./deploy.lock'));
+    if (file_exists(get('current_dir') . '/deploy.lock')) {
+        $deployLockFileContent = trim(file_get_contents(get('current_dir') . '/deploy.lock'));
         if (!empty($deployLockFileContent)) {
             $message = 'Deployment stopped! There is deploy.lock file with following message: ' . $deployLockFileContent;
         } else {
