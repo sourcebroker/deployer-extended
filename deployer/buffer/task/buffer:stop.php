0226a0c471b26ef619992b453c17de7f7ebab427
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-07-02 15:33:47 +0200

    [BUGFIX] Add missing error doc number.

--- deployer/buffer/task/buffer:stop.php
+++ deployer/buffer/task/buffer:stop.php
@@ -4,7 +4,7 @@ namespace Deployer;
 
 task('buffer:stop', function () {
     if (empty(get('deploy_path'))) {
-        throw new \RuntimeException('The "deploy_path" var is empty.');
+        throw new \RuntimeException('The "deploy_path" var is empty. [Error: 1499002321476]');
     }
     $releasesList = get('releases_list');
     // Remove lock files also from previous release because it can be still read by apache/nginx after switching.
