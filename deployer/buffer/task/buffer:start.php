efd5ab0580dfa573e2c9092f92c9a59f34e4dc45
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-07-02 15:09:39 +0200

    [!!!][FEATURE] Start entrypoints from deploy_path and not form web_path. Fix permissions lost after overwriting entrypoints.

--- deployer/buffer/task/buffer:start.php
+++ deployer/buffer/task/buffer:start.php
@@ -3,38 +3,48 @@
 namespace Deployer;
 
 task('buffer:start', function () {
+    if (empty(get('deploy_path'))) {
+        throw new \RuntimeException('The "deploy_path" var is empty. [Error: 1498989780841]');
+    }
+    if (empty(get('random'))) {
+        throw new \RuntimeException('The "random" var is empty. [Error: 1498989790330]');
+    }
+    if (preg_match('/^[a-zA-Z0-9]$/', get('random'))) {
+        throw new \RuntimeException('The "random" var should be only /^[a-zA-Z0-9]$/ because its used in filenames. 
+        [Error: 1498989790330]');
+    }
     $overwriteReleases = ['release', 'current'];
-    // Overwrite should be done in /release and /current
     foreach ($overwriteReleases as $overwriteRelease) {
-        $overwriteReleasePath = get('deploy_path') . '/' . $overwriteRelease;
-        if (test("[ -L $overwriteReleasePath ]")) {
+        $overwriteReleasePath = get('deploy_path') . '/' . $overwriteRelease . '/';
+        if (test("[ -e $overwriteReleasePath ]")) {
+            $tempPath = $overwriteReleasePath . get('random');
+            run('[ -e ' . $tempPath . ' ] || mkdir -p ' . $tempPath);
             $entrypointInjectStartComment = "\n\n// deployer extended buffering request code START\n";
             $entrypointInjectEndComment = "\n// deployer extended buffering request code END\n\n";
-
-            foreach (get('buffer_config') as $key => $buffer) {
-                if (!isset($buffer['entrypoint_filename'])) {
-                    throw new \RuntimeException('entrypoint_filename not set for buffer_data');
+            foreach ((array)get('buffer_config') as $key => $buffer) {
+                if (empty($buffer['entrypoint_filename'])) {
+                    throw new \RuntimeException('entrypoint_filename not set for buffer_data [Error: 1498990095089]');
                 }
                 $entrypointFilename = $buffer['entrypoint_filename'];
-                if (isset($buffer['entrypoint_needle'])) {
-                    $entrypointNeedle = $buffer['entrypoint_needle'];
-                } else {
+                if (empty($buffer['entrypoint_needle'])) {
                     $entrypointNeedle = '<?php';
-                }
-                if (isset($buffer['locker_filename'])) {
-                    $lockerFilename = $buffer['locker_filename'];
                 } else {
+                    $entrypointNeedle = $buffer['entrypoint_needle'];
+                }
+                if (empty($buffer['locker_filename'])) {
                     $lockerFilename = 'buffer.lock';
-                }
-                if (isset($buffer['entrypoint_inject'])) {
-                    $entrypointInject = $buffer['entrypoint_inject'];
                 } else {
+                    $lockerFilename = $buffer['locker_filename'];
+                }
+                if (empty($buffer['entrypoint_inject'])) {
                     $entrypointInject =
                         "isset(\$_SERVER['HTTP_X_DEPLOYER_DEPLOYMENT']) && \$_SERVER['HTTP_X_DEPLOYER_DEPLOYMENT'] == '{{random}}' ? \$deployerExtendedEnableBufferLock = false: \$deployerExtendedEnableBufferLock = true;\n"
                         . "isset(\$_ENV['DEPLOYER_DEPLOYMENT']) && \$_ENV['DEPLOYER_DEPLOYMENT'] == '{{random}}' ? \$deployerExtendedEnableBufferLock = false: \$deployerExtendedEnableBufferLock = true;\n"
                         . "while (file_exists(__DIR__ . '/$lockerFilename') && \$deployerExtendedEnableBufferLock) {\n    usleep(200000);\n    clearstatcache(true, __DIR__ . '/$lockerFilename');\n}";
+                } else {
+                    $entrypointInject = $buffer['entrypoint_inject'];
                 }
-                $entrypointFileContent = trim(run('cd {{release_path}} && [ -f {{web_path}}' . $entrypointFilename . ' ] && cat {{web_path}}' . $entrypointFilename . ' || echo ""')->toString());
+                $entrypointFileContent = trim(run('cd {{release_path}} && [ -f ' . $entrypointFilename . ' ] && cat ' . $entrypointFilename . ' || echo ""')->toString());
                 if (strpos($entrypointFileContent, $entrypointInjectStartComment) === false) {
                     if (!empty($entrypointFileContent)) {
                         $pos = strpos($entrypointFileContent, $entrypointNeedle);
@@ -43,22 +53,20 @@ task('buffer:start', function () {
                                 $entrypointNeedle .
                                 $entrypointInjectStartComment . $entrypointInject . $entrypointInjectEndComment, $pos,
                                 strlen($entrypointNeedle));
-                            $entrypointAbsoluteTmpPath = parse($overwriteReleasePath . '/{{web_path}}' . $entrypointFilename . '{{random}}');
-                            $entrypointAbsolutePath = parse($overwriteReleasePath . '/{{web_path}}' . $entrypointFilename);
-
-                            // cp -pt will create copy preserving the paermissions
-                            run('cp -pt ' . $entrypointAbsolutePath . ' ' . $entrypointAbsoluteTmpPath);
-                            run('echo ' . escapeshellarg($content) . ' > ' . $entrypointAbsoluteTmpPath);
-                            run('mv -T ' . $entrypointAbsoluteTmpPath . ' ' . $entrypointAbsolutePath);
+                            $entrypointAbsolutePath = $overwriteReleasePath . $entrypointFilename;
+                            run('cp -p ' . $entrypointAbsolutePath . ' ' . $tempPath . '/');
+                            run('echo ' . escapeshellarg($content) . ' > ' . $tempPath . '/' . basename($entrypointAbsolutePath));
+                            run('mv -T ' . $tempPath . '/' . basename($entrypointAbsolutePath) . ' ' . $entrypointAbsolutePath);
                         } else {
-                            throw new \RuntimeException('Can not find needle to inject with inclusion');
+                            throw new \RuntimeException('Can not find needle to inject with inclusion. [Error: 1498990135243]');
                         }
                     } else {
-                        throw new \RuntimeException('Can not find file to overwrite or the file is empty. File that was read is: ' . $overwriteReleasePath . '/' . get('web_path') . $entrypointFilename);
+                        throw new \RuntimeException('Can not find file to overwrite or the file is empty. File that was read is: ' . $overwriteReleasePath . '/' . $entrypointFilename . '. [Error: 1498990146197]');
                     }
                 }
                 run('cd ' . $overwriteReleasePath . ' && touch ' . (dirname($entrypointFilename) ? dirname($entrypointFilename) . '/' : '') . $lockerFilename);
             }
+            run('rmdir ' . $tempPath);
         }
     }
 })->desc('Start buffering reqests to application entrypoints.');
--- deployer/buffer/task/buffer:stop.php
+++ deployer/buffer/task/buffer:stop.php
@@ -3,6 +3,9 @@
 namespace Deployer;
 
 task('buffer:stop', function () {
+    if (empty(get('deploy_path'))) {
+        throw new \RuntimeException('The "deploy_path" var is empty.');
+    }
     $releasesList = get('releases_list');
     // Remove lock files also from previous release because it can be still read by apache/nginx after switching.
     // get('releases_list') is cached by deployer on first call in other task so it does not have the latest release
@@ -14,21 +17,17 @@ task('buffer:stop', function () {
     foreach ($overwriteReleases as $overwriteRelease) {
         $overwriteReleasePath = get('deploy_path') . '/' . $overwriteRelease;
         foreach (get('buffer_config') as $key => $buffer) {
-            if (!isset($buffer['entrypoint_filename'])) {
-                throw new \RuntimeException('entrypoint_filename not set for buffer_data');
+            if (empty($buffer['entrypoint_filename'])) {
+                throw new \RuntimeException('entrypoint_filename not set for buffer_data [Error: 1498988796776]');
             }
             $entrypointFilename = $buffer['entrypoint_filename'];
-            if (isset($buffer['locker_filename'])) {
-                $lockerFilename = $buffer['locker_filename'];
-            } else {
+            if (empty($buffer['locker_filename'])) {
                 $lockerFilename = 'buffer.lock';
-            }
-            if (dirname($entrypointFilename) != '.') {
-                $entrypointDirectory = dirname($entrypointFilename) . '/';
             } else {
-                $entrypointDirectory = '';
+                $lockerFilename = $buffer['locker_filename'];
             }
-            run('cd ' . $overwriteReleasePath . ' && rm -f {{web_path}}' . $entrypointDirectory . $lockerFilename);
+            $entrypointDirectory = dirname($entrypointFilename) === '.' ? '' : dirname($entrypointFilename) . '/';
+            run('cd ' . $overwriteReleasePath . ' && rm -f ' . $entrypointDirectory . $lockerFilename);
         }
     }
 })->desc('Stop buffering reqests to application entrypoints.');
