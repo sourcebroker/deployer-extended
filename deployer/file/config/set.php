ead00a6063a09164ca93675edc38f675b9f79907
Lukasz Dobrowolski <l00k@protonmail.com>
2019-02-28 16:37:59 +0100

    TASK Move Configuration class to Utility space, and InstanceUtility from database module

--- deployer/file/config/set.php
+++ deployer/file/config/set.php
@@ -2,12 +2,12 @@
 
 namespace Deployer;
 
-use SourceBroker\DeployerExtendedDatabase\Utility\InstanceUtility;
-use SourceBroker\DeployerExtended\Configuration;
+use SourceBroker\DeployerExtended\Utility\Instance;
+use SourceBroker\DeployerExtended\Utility\Configuration;
 
 // Returns path to store file backups on remote instance.
 set('file_backup_path', function () {
-    $currentInstance = (new InstanceUtility)->getCurrentInstance();
+    $currentInstance = (new Instance)->getCurrentInstance();
     $environment = Configuration::getEnvironment($currentInstance);
 
     if (!$environment->get('file_backup_path_relative', false)) {
--- src/Utility/Configuration.php
+++ src/Utility/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SourceBroker\DeployerExtended;
+namespace SourceBroker\DeployerExtended\Utility;
 
 use Deployer\Deployer;
 
