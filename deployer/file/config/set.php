2e043c9978c25114b998fccdd7f755d5fc696a0e
Lukasz Dobrowolski <l00k@protonmail.com>
2019-05-06 17:09:29 +0200

    [FEATURE] File backups - set proper deploy path

--- deployer/file/config/set.php
+++ deployer/file/config/set.php
@@ -7,13 +7,10 @@ use SourceBroker\DeployerInstance\Configuration;
 
 // Returns path to store file backups on remote instance.
 set('file_backup_path', function () {
-    $currentInstance = (new Instance)->getCurrentInstance();
-    $environment = Configuration::getEnvironment($currentInstance);
-
-    if (!$environment->get('file_backup_path_relative', false)) {
-        $path = $environment->get('deploy_path') . '/.dep/backups';
+    if (!get('file_backup_path_relative', false)) {
+        $path = get('deploy_path') . '/.dep/backups';
     } else {
-        $path = $environment->get('deploy_path') . '/' . $environment->get('file_backup_path_relative');
+        $path = get('deploy_path') . '/' . get('file_backup_path_relative');
     }
     run('[ -d ' . $path . ' ] || mkdir -p ' . $path);
     return $path;
