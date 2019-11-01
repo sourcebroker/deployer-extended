c19eff6bcd0649c2789b8c0c35ba3ed3599b4f20
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2019-11-01 15:34:18 +0100

    [TASK][BREAKING] Fix branch determination and change log filename.

--- deployer/deploy/task/deploy_check_branch.php
+++ deployer/deploy/task/deploy_check_branch.php
@@ -8,14 +8,26 @@ use Deployer\Exception\GracefulShutdownException;
 // Read more on https://github.com/sourcebroker/deployer-extended#deploy-check-branch
 task('deploy:check_branch', function () {
     cd('{{deploy_path}}');
-    if (run('if [ -f .dep/branch ]; then echo "true"; fi')->toBool()) {
-        $csv = run('tail -n 1 .dep/branch');
+    if (run('if [ -f .dep/logs ]; then echo "true"; fi')->toBool()) {
+        $csv = run('tail -n 1 .dep/logs');
         if ($csv) {
+            try {
+                $branch = runLocally('git rev-parse --abbrev-ref HEAD');
+            } catch (\Throwable $exception) {
+                $branch = null;
+            }
+            $branch = get('branch') ?: $branch;
+            // If option `branch` is set.
+            if (input()->hasOption('branch')) {
+                $inputBranch = input()->getOption('branch');
+                if (!empty($inputBranch)) {
+                    $branch = $inputBranch;
+                }
+            }
             $metainfo = Csv::parse($csv)[0];
             if (isset($metainfo[2]) && isset($metainfo[3])) {
                 $currentBranch = $metainfo[2];
                 $userName = $metainfo[3];
-                $branch = get('branch') ?: 'master';
                 if ($currentBranch != $branch) {
                     if (!askConfirmation(sprintf('On instance you deploy to there is currently branch "%s" deployed by "%s". ' .
                         'This branch is different than you trying to deploy "%s". Do you really want to continue?',
--- deployer/deploy/task/deploy_extend_log.php
+++ deployer/deploy/task/deploy_extend_log.php
@@ -9,7 +9,20 @@ task('deploy:extend_log', function () {
     cd('{{deploy_path}}');
     $csv = run('tail -n 1 .dep/releases');
     $metainfo = Csv::parse($csv)[0];
-    $metainfo[2] = get('branch') ?: 'master';
+    try {
+        $branch = runLocally('git rev-parse --abbrev-ref HEAD');
+    } catch (\Throwable $exception) {
+        $branch = null;
+    }
+    $branch = get('branch') ?: $branch;
+    // If option `branch` is set.
+    if (input()->hasOption('branch')) {
+        $inputBranch = input()->getOption('branch');
+        if (!empty($inputBranch)) {
+            $branch = $inputBranch;
+        }
+    }
+    $metainfo[2] = $branch;
     $metainfo[3] = runLocally('git config user.name')->toString();
-    run("echo '" . implode(',', $metainfo) . "' >> .dep/branch");
+    run("echo '" . implode(',', $metainfo) . "' >> .dep/logs");
 })->desc('Add custom information to the log file');
